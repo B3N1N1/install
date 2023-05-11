ea0cdd0eafde68fbb47725b4b96b9031c793c3b7
B3N1N1 <122571115+B3N1N1@users.noreply.github.com>
2023-05-10 23:56:17 -0300

    Add files via upload

---
+++ hysectools.sh
@@ -0,0 +1,27 @@
+apt install gobuster
+apt install nmap
+apt install sqlmap
+apt install openvpn
+apt install aircrack-ng
+apt install hydra
+apt install wpscan
+apt install metasploit-framework
+apt install enum4linux
+apt install hping3
+clear
+echo "PC HACKADO!!!"
+echo "PC HACKADO!!!"
+echo "PC HACKADO!!!"
+echo "PC HACKADO!!!"
+echo "PC HACKADO!!!"
+echo "PC HACKADO!!!"
+echo "PC HACKADO!!!"
+echo "PC HACKADO!!!"
+echo "PC HACKADO!!!"
+echo "PC HACKADO!!!"
+echo "PC HACKADO!!!"
+echo "PC HACKADO!!!"
+echo "PC HACKADO!!!"
+sleep 3
+clear
+echo "brinks seu viado"
